0061d7274f83e1ad72b3fd938a8087c95cee3428
SueB1718 <kpark651@hotmail.com>
2020-04-24 16:14:26 -0400

    add Login.java

--- src/DoCommit.java
+++
@@ -1,7 +0,0 @@
-public class DoCommit {
-
-    public static void main(String[] args) {
-
-        System.out.println("Practice makes perfect");
-    }
-}
---
+++ src/Login.java
@@ -0,0 +1,8 @@
+public class Login {
+
+    public static void main(String[] args) {
+
+
+        //will see you in Github
+    }
+}
