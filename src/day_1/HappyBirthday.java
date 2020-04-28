e48c15fdf1e8eb4fe6134137f9af4c12dbf215de
SueB1718 <64115705+SueB1718@users.noreply.github.com>
2020-04-28 15:52:04 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -4,5 +4,7 @@ public class HappyBirthday {
 
     public static void main(String[] args) {
         // celebrate birthday for B17/18
+        System.out.println("Happy Birthday B17 B18"); 
+        System.out.println("this line added from Github")
     }
 }
